db940e479e7798c9422a975454ef0ca2020b2f38
Adam Lang <citizenadam@hithub.com>
2023-03-02 17:19:54 -0600

    Removed line breaks from faq

--- main.go
+++ main.go
@@ -16,32 +16,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, `<h1>FAQ Page</h1>
-	<ul>
-		<li>
-			<h2>Is there a free version</h2>
-			Yes We have a 30 day trial!
-		</li>
-		<li>
-			<h2> What are your hours</h2>
-			Our hours are 24/7!
-		</li>
-		<li>
-			<h2>What is the support line?</h2>
-			The support line is 888-888-8888
-		</li>
-		<li>
-			<h2>What is the montly cost?</h2>
-			The montly cost is $10!
-		</li>
-		<li>
-			<h2>What is your company mission?</h2>
-			We live to serve.
-		</li>
-		<li>
-			<h2>Where is this site?</h2>
-			Not on Earth
-		</li>
-		</ul>	
+	<ul><li><h2>Free version?</h2>30-day trial available</li><li><h2>Hours?</h2>24/7</li><li><h2>Support line?</h2>888-888-8888</li><li><h2>Monthly cost?</h2>$10</li><li><h2>Mission?</h2>Live to serve</li><li><h2>Location?</h2>Not on Earth</li></ul>
 	`)
 }
 
