f74e03f3b625fd8579a351758da3791d8e458b53
Adam Lang <citizenadam@hithub.com>
2023-03-04 21:50:04 -0600

    xss changes

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	bio := `<script>alert("Haha, you have been hac0rd");</script>`
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to Pet Search</h1><p><a href=\"/contact\">Contact</a><p><a href=\"/faq\">faq</a>")
+	fmt.Fprint(w, "<h1>Welcome to Pet Search</h1><p>Bio:"+bio+"</p><p><a href=\"/contact\">Contact</a><p><a href=\"/faq\">faq</a>")
 
 }
 
@@ -30,6 +31,9 @@ func main() {
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3001", r)
+	http.ListenAndServe("localhost:3000", r)
 }
