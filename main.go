04231e2d77410d7f582e9f880d3e1a8c82e01080
Adam Lang <citizenadam@hithub.com>
2023-03-07 16:21:58 -0600

    added contact template and new template func

--- main.go
+++ main.go
@@ -11,9 +11,25 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	tplPath := filepath.Join("templates", "home.gohtml")
-	tpl, err := template.ParseFiles(tplPath)
+	executeTemplate(w, tplPath)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul><li><h2>Free version?</h2>30-day trial available</li><li><h2>Hours?</h2>24/7</li><li><h2>Support line?</h2>888-888-8888</li><li><h2>Monthly cost?</h2>$10</li><li><h2>Mission?</h2>Live to serve</li><li><h2>Location?</h2>Not on Earth</li></ul>
+	`)
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tplpath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplpath)
+}
+
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
 		log.Printf("Parsing template: %v", err)
 		http.Error(w, "There Was an error parsing the template.", http.StatusInternalServerError)
@@ -25,19 +41,6 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "There Was an error executing the template.", http.StatusInternalServerError)
 		return
 	}
-
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-	<ul><li><h2>Free version?</h2>30-day trial available</li><li><h2>Hours?</h2>24/7</li><li><h2>Support line?</h2>888-888-8888</li><li><h2>Monthly cost?</h2>$10</li><li><h2>Mission?</h2>Live to serve</li><li><h2>Location?</h2>Not on Earth</li></ul>
-	`)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>Contact Us: <a href=\"mailto:paul@paul.com\">paul@paul.com</a>.")
 }
 
 func svgHandler(w http.ResponseWriter, r *http.Request) {
