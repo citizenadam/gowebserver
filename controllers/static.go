5b81a15ab9d819547cf4ccfe184266625b692969
Adam Lang <citizenadam@hithub.com>
2023-03-21 12:05:11 -0500

    Addded Dynamic FAQ

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"views"
@@ -17,5 +18,32 @@ func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "can it be free?",
+			Answer:   "Yes there is a free version.",
+		},
+		{
+			Question: "What are the hours?",
+			Answer:   "super support 24 7",
+		},
+		{
+			Question: "how do i contact support?",
+			Answer:   "call or email us.",
+		},
+		{
+			Question: "Where is the office?",
+			Answer:   "OnMoonbase12",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
