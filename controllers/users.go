539195469261390a3c35104f0b0e037794a04a84
Adam Lang <citizenadam@hithub.com>
2023-04-01 13:11:57 -0500

    form updates

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -12,4 +13,14 @@ type Users struct {
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	u.Templates.New.Execute(w, nil)
+}
+
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(w, "password my balls ", r.FormValue("password"), "\n")
+	fmt.Fprint(w, "email: Is wrong ", r.PostForm.Get("email"), "\n")
 }
